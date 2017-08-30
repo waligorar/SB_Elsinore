4469e6cf209933604f38a08d207d61a138691996
rmw195 <rmw195@C02T1209G8WL.local>
2017-08-30 08:19:43 -0400

    migrating to home office

--- SB_Elsinore/src/com/sb/elsinore/LaunchControl.java
+++ SB_Elsinore/src/com/sb/elsinore/LaunchControl.java
@@ -1,12 +1,18 @@
 package com.sb.elsinore;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import com.sb.recipe.Recipe;
 
 public class LaunchControl {
 	/*
@@ -14,13 +20,29 @@ public class LaunchControl {
 	 */
     public int serverPort = 0;
 
-	private static Log log = LogFactory.getLog(BrewServer.class);
+	private static Log log = LogFactory.getLog(LaunchControl.class);
+	
+	private BrewServer bs = null;
 
 	public static void main(String[] args) {
 		log.debug("Main start");
 		
 		LaunchControl lc = new LaunchControl();
 		lc.startServer();
+		
+		try {
+			log.debug("main(): Begin unmashal");
+			JAXBContext jaxbContext = JAXBContext.newInstance(Customer.class);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			Customer customer = (Customer) jaxbUnmarshaller.unmarshal(new File("recipes/customer.xml"));
+			log.debug("main(): End unmashal");
+			log.debug("main(): Name = " + customer.getName());
+			log.debug("main(): Age = " + customer.getAge());
+			log.debug("main(): ID = " + customer.getId());
+		} catch (Exception ie) {
+			ie.printStackTrace();
+		}
+		
 		
 		log.debug("Main end");
 	}
@@ -49,11 +71,13 @@ public class LaunchControl {
 		log.debug("LaunchControl.startServer(): Begin");
 
 		try {
-			new BrewServer(this.serverPort);
+			if (null == bs) {
+				bs = new BrewServer(this.serverPort);
+			}
 		} catch (IOException ie) {
 			log.error("main() IOException: " + ie.getMessage());
 		}
-		
+
 		log.debug("LaunchControl.startServer(): End");
 	}
 	
---
+++ SB_Elsinore/src/com/sb/recipe/Recipe.java
@@ -0,0 +1,56 @@
+package com.sb.recipe;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Recipe {
+	private static final String sRecipeFolder = "recipes";
+	private String currentRecipe = "";
+	
+	public String getCurrentRecipe() {
+		return currentRecipe;
+	}
+
+	public void setCurrentRecipe(String currentRecipe) {
+		this.currentRecipe = currentRecipe;
+	}
+
+	private static Log log = LogFactory.getLog(Recipe.class);
+
+	public Recipe () {
+		log.debug("init() Start");
+		log.debug("init() End");
+	}
+	
+	public Recipe(Recipe r) {
+		log.debug("init(Recipe) Start");
+		log.debug("init(Recipe) End");
+	}
+	
+	public ArrayList<String> GetRecipeList() {
+		log.debug("GetRecipeList(): Begin");
+		
+		File folder = new File(sRecipeFolder);
+		File[] listOfFiles = folder.listFiles();
+		ArrayList<String> recipes = new ArrayList<String>();
+		
+		for (File file : listOfFiles) {
+			if (file.isFile()) {
+				int i = 1;
+				recipes.add(file.getName());
+				log.debug("File(" + i++ + ") = " + file.getName());
+			}
+		}
+
+		log.debug("GetRecipeList(): End");
+		return recipes;
+	}
+	
+	public void load() {
+		log.debug("load(): Begin");
+		log.debug("load(): End");
+	}
+}
